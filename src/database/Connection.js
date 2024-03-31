2a022838726f6d68b2bb488a8734e2b0d1666404
“SantiagoABM” <barbozamujica109@gmail.com>
2024-03-31 13:23:50 -0500

    cambio de base de datos

--- src/database/Connection.js
+++ src/database/Connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const MONGODB_URI = "mongodb://localhost:27017/Elzu";
-//const MONGODB_URI = `mongodb+srv://Tiago:tysonryx123@projects.dkslbjj.mongodb.net/Elzu?retryWrites=true&w=majority`
+//const MONGODB_URI = "mongodb://localhost:27017/Elzu";
+const MONGODB_URI = `mongodb+srv://Tiago:tysonryx123@projects.dkslbjj.mongodb.net/Elzu?retryWrites=true&w=majority`
 mongoose.connect(MONGODB_URI)
     .then(db => console.log('Conectado'))
     .catch(error => console.log('Desconectado'))
